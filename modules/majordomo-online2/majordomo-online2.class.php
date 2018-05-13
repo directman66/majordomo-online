4bfcc4374c116a540b496e4fdc670f46a01e843c
directman66 <sannikovdi@yandex.ru>
2018-05-13 09:29:07 +0500

    Create majordomo-online2.class.php

---
+++ modules/majordomo-online2/majordomo-online2.class.php
@@ -0,0 +1,342 @@
+<?php
+/**
+* https://livegpstracks.com/
+* @package project
+* @author Wizard <sergejey@gmail.com>
+* @copyright http://majordomo.smartliving.ru/ (c)
+* @version 0.1 (wizard, 09:04:00 [Apr 04, 2016])
+*/
+//
+//
+class online2 extends module {
+/**
+*
+* Module class constructor
+*
+* @access private
+*/
+function livegpstracks() {
+  $this->name="online2";
+  $this->title="majordomo-online2";
+  $this->module_category="<#LANG_SECTION_APPLICATIONS#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams($data=0) {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams() {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $out['TAB']=$this->tab;
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+ $this->getConfig();
+//        if ((time() - gg('cycle_online2Run')) < $this->config['ONLINE2_TIMEOUT']*2 ) {
+        if ((time() - gg('cycle_online2Run')) < 360*2 ) {
+			$out['CYCLERUN'] = 1;
+		} else {
+			$out['CYCLERUN'] = 0;
+		}
+ 
+ $out['UUID'] = $this->config['UUID'];
+ $out['SRV_NAME']=$this->config['SRV_NAME'];
+ $out['API_MAC']=$this->config['API_MAC'];
+ $out['API_SERVER']=$this->config['API_SERVER'];
+ $out['API_PORT']=$this->config['API_PORT'];
+ $out['EVERY']=$this->config['EVERY'];
+ 
+ if (!$out['UUID']) {
+	 $out['UUID'] = md5(microtime() . rand(0, 9999));
+	 $this->config['UUID'] = $out['UUID'];
+	 $this->saveConfig();
+ }
+ 
+ if ($this->view_mode=='update_settings') {
+	global $srv_name;
+	$this->config['SRV_NAME']=$srv_name;	 
+	global $api_server;
+	$this->config['API_SERVER']=$api_server;	 
+	global $api_port;
+	$this->config['API_PORT']=$api_port;	 
+	global $api_mac;
+	$this->config['API_MAC']=$api_mac;
+	global $every;
+	$this->config['EVERY']=$every;
+   
+   $this->saveConfig();
+   $this->redirect("?");
+ }
+ if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
+  $out['SET_DATASOURCE']=1;
+ }
+ 
+ //if ($this->tab=='' || $this->tab=='outdata') {
+if ($this->tab=='outdata') {
+   $this->outdata_search($out);
+ }  
+ //if ($this->tab=='indata') {
+if ($this->tab=='' || $this->tab=='indata') {	
+   $this->indata_search($out); 
+ }
+ if ($this->view_mode=='test') {
+setGlobal('cycle_livegpstracksControl','start'); 	 
+		$this->readData();
+ }
+ if ($this->view_mode=='outdata_edit') {
+   $this->outdata_edit($out, $this->id);
+ }
+ if ($this->view_mode=='outdata_del') {
+   $this->outdata_del($this->id);
+   $this->redirect("?data_source=$this->data_source&view_mode=node_edit&id=$pid&tab=outdata");
+ }	
+ if ($this->view_mode=='indata_edit') {
+   $this->indata_edit($out, $this->id);
+ }
+ if ($this->view_mode=='indata_del') {
+   $this->indata_del($this->id);
+   $this->redirect("?data_source=$this->data_source&view_mode=node_edit&id=$pid&tab=indata");
+ }	
+}
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ $this->admin($out);
+}
+/**
+* OutData search
+*
+* @access public
+*/
+ function outdata_search(&$out) {	 
+  require(DIR_MODULES.$this->name.'/outdata.inc.php');
+ }
+/**
+* InData search
+*
+* @access public
+*/ 
+ function indata_search(&$out) {	 
+  require(DIR_MODULES.$this->name.'/indata.inc.php');
+ }
+/**
+* OutData edit/add
+*
+* @access public
+*/
+ function outdata_edit(&$out, $id) {	
+  require(DIR_MODULES.$this->name.'/outdata_edit.inc.php');
+ } 
+/**
+* OutData delete record
+*
+* @access public
+*/
+ function outdata_del($id) {
+  $rec=SQLSelectOne("SELECT * FROM lgps_out WHERE ID='$id'");
+  // some action for related tables
+  SQLExec("DELETE FROM lgps_out WHERE ID='".$rec['ID']."'");
+ }
+/**
+* InData edit/add
+*
+* @access public
+*/
+ function indata_edit(&$out, $id) {	
+  require(DIR_MODULES.$this->name.'/indata_edit.inc.php');
+ } 
+/**
+* InData delete record
+*
+* @access public
+*/
+ function indata_del($id) {
+  $rec=SQLSelectOne("SELECT * FROM lgps_in WHERE ID='$id'");
+  // some action for related tables
+  SQLExec("DELETE FROM lgps_in WHERE ID='".$rec['ID']."'");
+ }
+ 
+ function propertySetHandle($object, $property, $value) {
+   $this->getConfig();
+   $table='lgps_out';
+   $properties=SQLSelect("SELECT ID FROM $table WHERE LINKED_OBJECT LIKE '".DBSafe($object)."' AND LINKED_PROPERTY LIKE '".DBSafe($property)."'");
+   $total=count($properties);
+   if ($total) {
+    for($i=0;$i<$total;$i++) {
+     //to-do
+    }
+   }
+ }
+ function processCycle() {
+   $this->getConfig();
+   $every=$this->config['EVERY'];
+   $tdev = time()-$this->config['LATEST_UPDATE'];
+   $has = $tdev>$every*60;
+//   if ($tdev < 0) {
+		$has = true;
+//   }
+   
+   if ($has) {     
+	$this->readData();
+		 
+	$this->config['LATEST_UPDATE']=time();
+	$this->saveConfig();
+   } 
+ }
+ 
+ 
+ 	
+ 
+   
+ 
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($data='') {
+  parent::install();
+ }
+/**
+* Uninstall
+*
+* Module uninstall routine
+*
+* @access public
+*/
+ function uninstall() {
+  SQLExec('DROP TABLE IF EXISTS online2');
+  
+  parent::uninstall();
+ }
+/**
+* dbInstall
+*
+* Database installation routine
+*
+* @access private
+*/
+ function dbInstall($data) {
+/*
+nm_outdata - 
+*/
+addClass('online'); 
+  
+  $data = <<<EOD
+online2: IPADDR varchar(100)
+online2: HOSTNAME varchar(100) 
+online2: RESULT int(30)
+online2: SEARCH_WORD varchar(255) NOT NULL DEFAULT ''
+online2: CHECK_LATEST datetime
+ online2: CHECK_NEXT datetime
+ online2: SCRIPT_ID_ONLINE int(10) NOT NULL DEFAULT '0'
+ online2: CODE_ONLINE text
+ online2: SCRIPT_ID_OFFLINE int(10) NOT NULL DEFAULT '0'
+ online2: CODE_OFFLINE text
+ online2: OFFLINE_INTERVAL int(10) NOT NULL DEFAULT '0'
+ online2: ONLINE_INTERVAL int(10) NOT NULL DEFAULT '0'
+ online2: LINKED_OBJECT varchar(255) NOT NULL DEFAULT ''
+ online2: LINKED_PROPERTY varchar(255) NOT NULL DEFAULT ''
+ online2: COUNTER_CURRENT int(10) NOT NULL DEFAULT '0'
+ online2: COUNTER_REQUIRED int(10) NOT NULL DEFAULT '0'
+ online2: STATUS_EXPECTED int(3) NOT NULL DEFAULT '0'
+ online2: LOG text
+EOD;
+  parent::dbInstall($data);
+setGlobal('cycle_online2AutoRestart','1');	 
+	 
+ }
+// --------------------------------------------------------------------
+//////
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgQXByIDA0LCAyMDE2IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
